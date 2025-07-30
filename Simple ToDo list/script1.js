873a8abfb454bcbfcbb361ac6a7e78be729c3dd3
Rahul kumar sah <rahulraj12777@gmail.com>
2025-07-30 18:57:15 +0530

    to do list

---
+++ Simple ToDo list/script1.js
@@ -0,0 +1,18 @@
+document.querySelector('#push').onclick=function(){
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert("Please Enter a Task ")
+    }
+else {
+    document.querySelector("#tasks").innerHTML += `
+    <div class="task">
+    <span id ="taskname">
+    ${document.querySelector("#newtask input").value}
+    </spam>
+    </div>
+    
+    `;
+  }
+};
+
+
+
