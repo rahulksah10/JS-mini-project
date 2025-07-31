16b9a175392585c38d66e31a25e2dc9ef8bd5771
Rahul kumar sah <rahulraj12777@gmail.com>
2025-07-31 16:03:54 +0530

    fixing bugs

--- Simple ToDo list/script1.js
+++ Simple ToDo list/script1.js
@@ -1,18 +1,31 @@
-document.querySelector('#push').onclick=function(){
-    if(document.querySelector('#newtask input').value.length == 0){
-        alert("Please Enter a Task ")
-    }
-else {
+document.querySelector("#push").onclick = function () {
+  if (document.querySelector("#newtask input").value.length == 0) {
+    alert("Please Enter a Task ");
+  } else {
     document.querySelector("#tasks").innerHTML += `
     <div class="task">
     <span id ="taskname">
     ${document.querySelector("#newtask input").value}
-    </spam>
+    </span>
+    <button class="delete">
+       <i class="fa-solid fa-trash"></i> 
+    </button>
     </div>
     
     `;
+
+    var current_tasks = document.querySelectorAll(".delete");
+    for (var i = 0; i < current_tasks.length; i++) {
+      current_tasks[i].onclick = function () {
+        this.parentNode.remove();
+      };
+    }
+    var task = document.querySelectorAll(".task");
+    for (var i = 0; i < task.length; i++) {
+      task[i].onclick = function () {
+        this.classList.toggle("complete");
+      };
+    }
+    document.querySelector("#newtask input").value = "";
   }
 };
-
-
-
