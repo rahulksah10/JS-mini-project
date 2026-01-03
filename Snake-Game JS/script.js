655f6fe9d660690d729877191129fefa1b053085
Rahul kumar sah <rahulraj12777@gmail.com>
2026-01-04 00:35:46 +0530

    features

---
+++ Snake-Game JS/script.js
@@ -0,0 +1,15 @@
+const board = document.querySelector(".board");
+const blockHeight = 30;
+const blockWidth = 30;
+
+const cols = Math.floor(board.clientWidth / blockWidth);
+const rows = Math.floor(board.clientHeight / blockHeight);
+
+
+for (let row = 0; row < rows; row++) {
+  for (let col = 0; col < cols; col++) {
+        const block = document.createElement("div");
+        block.classList.add("block");
+        board.appendChild(block);
+  }
+}
